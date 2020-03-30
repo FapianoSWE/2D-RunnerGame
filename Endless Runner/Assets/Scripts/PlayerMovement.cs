33e4baef5e6895e9c43535df48d22625a8d950e7
FapianoSWE <33018606+FapianoSWE@users.noreply.github.com>
2020-03-30 17:12:26 +0200

    player and cameramovement

---
+++ Endless Runner/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float movementspeed = 5f;
+    public float jumpforce = 5f;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+        {
+            transform.position += new Vector3(-1 * Time.deltaTime * movementspeed, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+        {
+            transform.position += new Vector3(1 * Time.deltaTime * movementspeed, 0, 0);
+        }
+        if(Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W))
+        {
+            Rigidbody2D rb = GetComponent<Rigidbody2D>();
+            rb.AddForce(new Vector2(0, 50 * jumpforce));
+        }
+    }
+}
